adc9e95a823621d5782b5bd692b7456a4286427e
Hadi <hadi@debian>
2017-01-20 01:06:48 +0100

    dox

--- src/undostack.cpp
+++ src/undostack.cpp
@@ -1,14 +1,46 @@
+/*!
+ * \file undostack.cpp
+ * \brief Implementation of the methods of the UndoStack class declared in the undostack.hpp header.
+ * \author Jérémi Bernard
+ *         Benjamin De Pourquery
+ *         Rémy Maugey
+ *         Hadrien Decoudras
+ * \version 0.2
+ */
+
 #include "undostack.hpp"
 #include "imagewidget.hpp"
 
-#include <QDebug>
-
+/**************************************************************
+ **************************************************************
+ *
+ * Constructor
+ *
+ **************************************************************/
 UndoStack::UndoStack() {}
 
+/**************************************************************
+ **************************************************************
+ *
+ * Constructor
+ *
+ **************************************************************/
 UndoStack::UndoStack(ImageWidget* lWidget, ImageWidget* rWidget) : lWidget(lWidget), rWidget(rWidget) {}
 
+/**************************************************************
+ **************************************************************
+ *
+ * Destructor
+ *
+ **************************************************************/
 UndoStack::~UndoStack() {}
 
+/**************************************************************
+ **************************************************************
+ *
+ * Undo
+ *
+ **************************************************************/
 void UndoStack::undo()
 {
     if(undoStack.empty())
